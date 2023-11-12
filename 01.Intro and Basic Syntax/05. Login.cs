5802116227bd5a909eb6c45a992dbba56f4d2bb5
Kolev <kolevaz@gmail.com>
2023-11-12 22:45:40 +0200

    Adding 05,06

---
+++ 01.Intro and Basic Syntax/05. Login.cs
@@ -0,0 +1,42 @@
+﻿namespace SoftUni_CSharp_Fundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string pass = "";
+
+            int count = 0;
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                pass += name[i];
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input != pass)
+                {
+                    count++;
+
+                    if (count == 4)
+                    {
+                        Console.WriteLine($"User {name} blocked!");
+                        break;
+                    }
+
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+
+                else
+                {
+                    Console.WriteLine($"User {name} logged in.");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 01.Intro and Basic Syntax/06. Strong number.cs
@@ -0,0 +1,23 @@
+﻿namespace SoftUni_CSharp_Fundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = num; i > 0; i = i / 10)
+            {
+                int digit = i % 10;
+                int factorial = 1;
+                for (int j = 1; j <= digit; j++)
+                {
+                    factorial = factorial * j;
+                }
+                sum = sum + factorial;
+            }
+            if (sum == num) Console.WriteLine("yes");
+            else Console.WriteLine("no");
+        }
+    }
+}
